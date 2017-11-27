ece387bcd2fc77669b86884f768e850c68291733
JavAlisson <javalisson@gmail.com>
2017-11-27 13:45:42 -0200

    Commit inicial

---
+++ javalisson/script.js
@@ -0,0 +1,24 @@
+/**
+ * p5js boilerplate
+ * 
+ * 
+ */
+
+// utilizada para carregar imagens, sons e fontes
+function preload() {
+    
+}
+
+// chamada no inicio do programa
+function setup() {
+    // cria o quadro, com dimensoes 900 x 400
+    createCanvas(900, 400);
+}
+
+// chamada toda vez que o quadro for redesenhado
+// ou seja, a cada frame da animacao
+function draw() {
+    // pinta o fundo de preto
+    background(0, 0, 0);
+
+}
