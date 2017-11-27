1811bf224b661a6c84367d0d7912c7a12bfb866a
0ddts0 <34035356+0ddts0@users.noreply.github.com>
2017-11-27 15:22:29 -0200

    Update script.js

--- danilo/script.js
+++ danilo/script.js
@@ -16,6 +16,10 @@
 	var lista4 = [];
 	var angulo;
 
+	var vento = 0;
+
+	var pos_atual_mouse = 0, pos_anterior_mouse = 0;
+
 function preload() {
 
 	cenario = loadImage('cenario.png');
@@ -34,6 +38,8 @@ function preload() {
 }
 
 function setup() {
+	noCursor();
+
 	createCanvas(1200, 700);
 
 	posestrela = createVector(1400, 1);
@@ -54,12 +60,20 @@ function setup() {
 		lista4.push(cria_floco4(random(0, width), random(0, height), (random(1, 10) / 5)));
 	}
 
+	angulo = PI/3.0;
 
 	tocaMusica();
+
+	
 }
 
-
 function draw() {
+	pos_atual_mouse = mouseX;
+
+	if (pos_atual_mouse > pos_anterior_mouse) vento = 1;
+	else if (pos_atual_mouse < pos_anterior_mouse) vento = -1;
+	else vento = 0;
+
 	background(cenario);
 
 	image(boneco, 200, 420, 150, 200);
@@ -79,23 +93,25 @@ function draw() {
 
 	for (var i = 0; i < lista1.length; i++) {
 		desenha_floco1(lista1[i]);
-		atualiza_posicao_floco1(lista1[i]);
+		atualiza_posicao_floco1(lista1[i], vento);
 	}
 
 	for (var i = 0; i < lista2.length; i++) {
 		desenha_floco2(lista2[i]);
-		atualiza_posicao_floco2(lista2[i]);
+		atualiza_posicao_floco2(lista2[i], vento);
 	}
 
 	for (var i = 0; i < lista3.length; i++) {
 		desenha_floco3(lista3[i]);
-		atualiza_posicao_floco3(lista3[i]);
+		atualiza_posicao_floco3(lista3[i], vento);
 	}
 
 	for (var i = 0; i < lista4.length; i++) {
 		desenha_floco4(lista4[i]);
-		atualiza_posicao_floco4(lista4[i]);
+		atualiza_posicao_floco4(lista4[i], vento);
 	}
+
+	pos_anterior_mouse = mouseX;
 }
 
 function tocaMusica() {
@@ -143,45 +159,77 @@ function cria_floco4(x, y, v) {
 
 function desenha_floco1(floco) {
 
-	image(floco1, floco.x, floco.y, 50, 50);
+	push();
+	translate(floco.x, floco.y);
+	rotate(angulo);
+	angulo -= 0.001;
+	image(floco1,-25, -25, 50, 50);
+	pop();
 
 }
 
 function desenha_floco2(floco) {
 
-	image(floco2, floco.x, floco.y, 50, 50);
+	push();
+	translate(floco.x, floco.y);
+	rotate(angulo);
+	angulo -= 0.001;
+	image(floco2,-25, -25, 50, 50);
+	pop();
 
 }
 
 function desenha_floco3(floco) {
 
-	image(floco3, floco.x, floco.y, 50, 50);
+	push();
+	translate(floco.x, floco.y);
+	rotate(angulo);
+	angulo -= 0.001;
+	image(floco3,-25, -25, 50, 50);
+	pop();
 
 }
 
 function desenha_floco4(floco) {
 
-	image(floco4, floco.x, floco.y, 50, 50);
+	push();
+	translate(floco.x, floco.y);
+	rotate(angulo);
+	angulo -= 0.001;
+	image(floco4,-25, -25, 50, 50);
+	pop();
 
 }
 
 
-function atualiza_posicao_floco1(floco) {
+function atualiza_posicao_floco1(floco, vento) {
 	floco.y += floco.v;
+	floco.x += vento;
 	if (floco.y > height) floco.y = -10;
+	if (floco.x > width + 50) floco.x = -50;
+	if (floco.x < -50) floco.x = width + 50;
 }
 
 function atualiza_posicao_floco2(floco) {
 	floco.y += floco.v;
+	floco.x += vento;
 	if (floco.y > height) floco.y = -10;
+	if (floco.x > width + 50) floco.x = -50;
+	if (floco.x < -50) floco.x = width + 50;
 }
 
 function atualiza_posicao_floco3(floco) {
 	floco.y += floco.v;
+	floco.x += vento;
 	if (floco.y > height) floco.y = -10;
+	if (floco.x > width + 50) floco.x = -50;
+	if (floco.x < -50) floco.x = width + 50;
 }
 
 function atualiza_posicao_floco4(floco) {
 	floco.y += floco.v;
+	floco.x += vento;
 	if (floco.y > height) floco.y = -10;
+	if (floco.x > width + 50) floco.x = -50;
+	if (floco.x < -50) floco.x = width + 50;
 }
